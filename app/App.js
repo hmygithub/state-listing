cef2ceb8824eb920bcf1382991204e868f2fd870
hmygithub <2713237776@qq.com>
2017-10-17 14:41:56 +0800

    写一个名为 Calculator 的组件

--- app/App.js
+++ app/App.js
@@ -3,6 +3,30 @@
  */
 import React from "react"
 
+class Calculator extends React.Component{
+    //构造函数
+    constructor(props){
+        super(props)
+        this.state = {
+            temperature:''
+        }
+        this.handleChange=this.handleChange.bind(this)
+    }
+    handleChange(e){
+        this.setState({temperature: e.target.value})
+    }
+    render(){
+        const temperature = this.state.temperature;
+        return (
+            <fieldset>
+                <legend>输入一个摄氏温度</legend>
+                <input value={temperature} onChange={this.handleChange} />
+                <BoilingVerdict celsius={parseFloat(temperature)}/>
+            </fieldset>
+        )
+    }
+}
+
 function BoilingVerdict(props){//沸腾的 判决
     if(props.celsius >= 100){
         return <p>水会烧开</p>
@@ -10,3 +34,5 @@ function BoilingVerdict(props){//沸腾的 判决
         return <p>水不会烧开</p>
     }
 }
+
+export default Calculator
