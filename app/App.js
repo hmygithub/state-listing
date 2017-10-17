48afab97e04e3613a3750c7c9af83c4fdecda748
hmygithub <2713237776@qq.com>
2017-10-17 14:55:22 +0800

    修改Calculator，渲染两个不同的温度输入框框

--- app/App.js
+++ app/App.js
@@ -4,34 +4,14 @@
 import React from "react"
 
 class Calculator extends React.Component{
-    //构造函数
-    constructor(props){
-        super(props)
-        this.state = {
-            temperature:''
-        }
-        this.handleChange=this.handleChange.bind(this)
-    }
-    handleChange(e){
-        this.setState({temperature: e.target.value})
-    }
-    render(){
-        const temperature = this.state.temperature;
-        return (
-            <fieldset>
-                <legend>输入一个摄氏温度</legend>
-                <input value={temperature} onChange={this.handleChange} />
-                <BoilingVerdict celsius={parseFloat(temperature)}/>
-            </fieldset>
-        )
-    }
-}
 
-function BoilingVerdict(props){//沸腾的 判决
-    if(props.celsius >= 100){
-        return <p>水会烧开</p>
-    }else{
-        return <p>水不会烧开</p>
+    render(){
+        return (
+            <div>
+                <TemperatureInput scale="c" />
+                <TemperatureInput scale="f" />
+            </div>
+        )
     }
 }
 
