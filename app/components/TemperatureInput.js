3f3949992df91d0533e706f6d1db35b4e83de908
hmygithub <2713237776@qq.com>
2017-10-17 18:15:09 +0800

    将处理数据的逻辑交给Calculator处理

--- app/components/TemperatureInput.js
+++ app/components/TemperatureInput.js
@@ -7,6 +7,8 @@ const scaleNames = {
     c: 'Celsius',
     f: 'Fahrenheit'
 }
+
+// 相当于 view组件
 class TemperatureInput extends React.Component{
     //构造函数
     constructor(props){
@@ -24,40 +26,11 @@ class TemperatureInput extends React.Component{
             <fieldset>
                 <legend>Enter temperature in {scaleNames[scale]}:</legend>
                 <input value={temperature} onChange={this.handleChange} />
-                <BoilingVerdict celsius={parseFloat(temperature)}/>
             </fieldset>
         )
     }
 }
 
-function BoilingVerdict(props) {
-    if (props.celsius >= 100) {
-        return <p>水会烧开</p>;
-    }
-    return <p>水不会烧开</p>;
-}
-
-// 将华氏温度转为摄氏温度
-function toCelsius(fahrenheit){
-    return (fahrenheit - 32) * 5 / 9
-}
-
-// 将摄氏温度转为华氏温度
-function tofahrenheit(celsius){
-    return celsius *  9 / 5 + 32
-}
-
-function tryConvert(temperature, convert) {
-    const input = parseFloat(temperature);
-    if (Number.isNaN(input)) {
-        return '';
-    }
-    //转换后的结果
-    const output = convert(input);
-    //Math.round() 对值四舍五入
-    const rounded = Math.round(output * 1000) / 1000;
-    return rounded.toString();
-}
 
 //向外暴露
 export default TemperatureInput
