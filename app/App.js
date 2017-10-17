c14c55aeaddf5e8269fe3e0db212b375f25e31f6
hmygithub <2713237776@qq.com>
2017-10-17 14:39:28 +0800

    创建BoilingVerdict组件

--- app/App.js
+++ app/App.js
@@ -3,3 +3,10 @@
  */
 import React from "react"
 
+function BoilingVerdict(props){//沸腾的 判决
+    if(props.celsius >= 100){
+        return <p>水会烧开</p>
+    }else{
+        return <p>水不会烧开</p>
+    }
+}
