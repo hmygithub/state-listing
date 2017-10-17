2c392ea65b597148f3305402880c527c8ac0212c
hmygithub <2713237776@qq.com>
2017-10-17 16:38:25 +0800

    状态提升

--- app/App.js
+++ app/App.js
@@ -2,14 +2,49 @@
  * Created by lenovo on 2017/9/20.
  */
 import React from "react"
+import TemperatureInput from './components/TemperatureInput.js'
 
 class Calculator extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            temperature: '',
+            scale: 'c'
+        }
+        this.handleCelsiusChange=this.handleCelsiusChange.bind(this)
+        this.handleFahrenheitChange=this.handleFahrenheitChange.bind(this)
+    }
 
+    handleCelsiusChange(temperature){
+        this.setState = {
+            temperature,
+            scale: 'c'
+        }
+    }
+
+    handleFahrenheitChange(temperature){
+        this.setState = {
+            temperature,
+            scale: 'f'
+        }
+    }
     render(){
+        const scale = this.state.scale
+        const temperature = this.state.temperature
+        const celsius = scale === 'f' ? tryConvert(temperature, toCelsius): temperature
+        const fahrenheit = scale === 'c'? tryConvert(temperature, toFahrenheit): temperature
         return (
             <div>
-                <TemperatureInput scale="c" />
-                <TemperatureInput scale="f" />
+                <TemperatureInput scale="c"
+                                  temperature = {celsius}
+                                  onTemperatureChange = {this.handleCelsiusChange}
+                    />
+                <TemperatureInput scale="f"
+                                  temperature = {fahrenheit}
+                                  onTemperatureChange = {this.handleFahrenheitChange}
+                    />
+                <BoilingVerdict
+                    celsius={parseFloat(celsius)} />
             </div>
         )
     }
